97459e1f6b07dadd4f0aa6afbf506315cb2e883d
Eyal Yemini <student@mail.com>
2026-08-18 13:02:56 +0000

    fixed .f

--- hw/sudx_basic/sudx_basic.f
+++ hw/sudx_basic/sudx_basic.f
@@ -1,6 +1,4 @@
 +define+HOST_REGS_INTRF
-+incdir+$K5X_SUD_ROOT/sw/apps/sudx_basic
-$K5X_SUD_ROOT/hw/sudx_basic/sudx_basic_def_pkg.sv
-$K5X_SUD_ROOT/hw/sudx_basic/sudx_basic.sv
-
-
++incdir+$K5_SW_APPS/sudx_basic
+$MY_K5_XLRS/sudx_basic/sudx_basic_def_pkg.sv
+$MY_K5_XLRS/sudx_basic/sudx_basic.sv
